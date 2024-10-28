0bab7e5e08d92a45f34096e39c1a5989a60a7689
Anand Uzer <anandjx6@gmail.com>
2024-10-28 13:05:34 +0600

    changed

---
+++ contact.js
@@ -0,0 +1,15 @@
+function pop() {
+    alert("You have successfully submitted.");
+   
+}
+nameValue='';
+emailvalue='';
+messageValue='';
+
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let message= document.getElementById("message");
+
+nameValue= name.value;
+emailvalue=emai.value;
+messageValue=message.value;
